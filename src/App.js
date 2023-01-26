96bb2d060d06b6c22d0ae04ff98d6204fad49380
Pallab Majumdar <kitkatpallab@gmail.com>
2023-01-27 00:44:18 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+let name = "pallab";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+
+      <div className="container">
+        <h1>Hello {name}</h1>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsum
+        sunt iure veniam ex exercitationem! Eos ad omnis facilis alias. Maiores
+        quas asperiores nam et odio natus eveniet inventore tempora. Ab aliquid
+        quidem totam consequatur. Expedita a in ab et illum omnis, ducimus sequi
+        vero optio cupiditate animi totam, voluptatibus excepturi velit. Debitis
+        quos reiciendis tenetur in? Aliquid dignissimos totam quia, ipsam iste
+        molestiae? Nesciunt laboriosam voluptate quasi, beatae, alias illo
+        mollitia dolorem recusandae deserunt adipisci velit commodi, culpa quis
+        iure neque. Eum, qui non similique earum, nihil adipisci neque officiis
+        impedit aliquam doloremque fuga asperiores atque repudiandae! Neque,
+        omnis.
+      </div>
+    </>
   );
 }
 
