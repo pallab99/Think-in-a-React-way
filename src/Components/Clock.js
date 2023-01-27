0645da314d6e3477e973c4cbb4d0d4212542c64a
Pallab Majumdar <kitkatpallab@gmail.com>
2023-01-27 19:26:30 +0600

    React_State_And_Lifecycle

---
+++ src/Components/Clock.js
@@ -0,0 +1,30 @@
+import React from "react";
+// Functional Component
+
+// function clock({ locales }) {
+//   return (
+//     <h1 className="heading">
+//       <span className="text">
+//         Hello {new Date().toLocaleTimeString(locales)}</span>
+//     </h1>
+//   );
+// }
+
+// ReactDOM.render(<clock locales="bn-BD" />, document.getElementById("root"));
+
+//Class components
+// !Class a kono property send korle seta props r moddhe thake.Oita access korar jonno this.props.property name diye access kora jai
+//! Class r vitore props k change kora jabe na korle seta render function r moddhe infinite loop a porbe barbar render hobe.Props sob somoy baire theke change hobe
+class Clock extends React.Component {
+  render() {
+    return (
+      <h1 className="heading">
+        <span className="text">
+          Hello {new Date().toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    );
+  }
+}
+
+export default Clock;
