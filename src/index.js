089b909dac9a1788e66fe7b54f70aec9cb8aa60e
Pallab Majumdar <kitkatpallab@gmail.com>
2023-01-27 14:04:04 +0600

    React_Components_Props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// Functional Component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function clock({ locales }) {
+//   return (
+//     <h1 className="heading">
+//       <span className="text">
+//         Hello {new Date().toLocaleTimeString(locales)}</span>
+//     </h1>
+//   );
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(<clock locales="bn-BD" />, document.getElementById("root"));
+
+//Class components
+// !Class a kono property send korle seta props r moddhe thake.Oita access korar jonno this.props.property name diye access kora jai
+//! Class r vitore props k change kora jabe na korle seta render function r moddhe infinite loop a porbe barbar render hobe.Props sob somoy baire theke change hobe
+class Clock extends React.Component {
+  render() {
+    return (
+      <h1 className="heading">
+        <span className="text">
+          Hello - {this.props.children} {new Date().toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    );
+  }
+}
+
+ReactDOM.render(<Clock locale="bn-BD" >Pallab</Clock>, document.getElementById("root"));
