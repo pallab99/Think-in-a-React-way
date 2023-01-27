98b76c04ce934235aaafd95f67ca0c3f88ce0b5a
Pallab Majumdar <kitkatpallab@gmail.com>
2023-01-27 21:21:53 +0600

    final commit

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -16,11 +16,31 @@ import React from "react";
 // !Class a kono property send korle seta props r moddhe thake.Oita access korar jonno this.props.property name diye access kora jai
 //! Class r vitore props k change kora jabe na korle seta render function r moddhe infinite loop a porbe barbar render hobe.Props sob somoy baire theke change hobe
 class Clock extends React.Component {
+  state = { date: new Date() };
+
+  //!Jdi props r kaj na thake tahole constructor a lekhar dorkar nai
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { date: new Date() };
+  // }
+
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+
+  tick() {
+    this.setState({ date: new Date() }); //! this.state.date = new Date() - never do that
+  }
+
   render() {
     return (
       <h1 className="heading">
         <span className="text">
-          Hello {new Date().toLocaleTimeString(this.props.locale)}
+          Hello {this.state.date.toLocaleTimeString(this.props.locale)}
         </span>
       </h1>
     );
